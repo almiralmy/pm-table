b8d4339605598e441372bf216c2f0d95cce64d3e
almiralmy <almiralmy1984@gmail.com>
2016-11-28 18:13:09 +0100

    Removed cancel button

--- src/app/row.service.ts
+++ src/app/row.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import { Headers, Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 import { Row } from './row';
 import { ROWS } from './mock-rows';
 
 @Injectable()
 export class RowService {
+
+  private rowsUrl = '';
+  private headers = new Headers({'Content-Type':'application/json'});
+
+  constructor(
+    private http: Http
+  ){}
+
   getRows(): Promise<Row[]>{
     return Promise.resolve(ROWS);
+  }
+
+  create(wbscode: string): Promise<Row> {
+    return this.http
+      .post(this.rowsUrl, JSON.stringify({wbscode: wbscode}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+  /*
+  delete(id: number): Promise<void> {
+    const url = `${this.rowsUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+  */
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); //for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
--- src/app/rows.component.ts
+++ src/app/rows.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { Row } from './row';
 import { RowService } from './row.service';
 
@@ -24,5 +24,28 @@ export class RowsComponent implements OnInit {
   ngOnInit(): void {
     this.getRows();
   }
+
+  onSelect(row: Row): void {
+    this.selectedRow = row;
+  }
+
+  add(wbscode: string): void {
+    wbscode = wbscode.trim();
+    if(!wbscode) {return;}
+    this.rowService.create(wbscode)
+      .then(row => {
+        this.rows.push(row);
+        this.selectedRow = null;
+      });
+  }
+
+  /*delete(row: Row): void {
+    this.rowService
+      .delete(row.id)
+      .then(() => {
+        this.rows = this.rows.filter(h => h !== row);
+        if (this.selectedRow === row) { this.selectedRow = null; }
+      });
+  }*/
 
 }
